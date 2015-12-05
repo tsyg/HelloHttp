8677e452b0b72d563956ce3b6183686b087f607b
Yuri Tsyganenko <yuri.tsyganenko@dataart.com>
2015-12-05 18:23:52 +0300

    Sample HTTP hello app

---
+++ src/main/java/com/dataart/demo/hello.java
@@ -0,0 +1,42 @@
+// See  original code in http://stackoverflow.com/questions/3732109/simple-http-server-in-java-using-only-java-se-api#3732328
+
+package com.dataart.demo;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class hello {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new MyHandler());
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+	static int cnt=0;
+	
+	public String getMessage() {
+		return String.format("Hello, HTTP!  Count=%d\n",cnt);
+	}
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+	    cnt++;	
+	    // Debug
+	    System.out.println( String.format("Running hello! received requests: %d",cnt) );
+		
+            String response = getMessage();
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+}
---
+++ src/test/java/com/dataart/demo/AppTest.java
@@ -0,0 +1,54 @@
+package com.dataart.demo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import com.dataart.demo.hello;
+/**
+ * Unit test for the App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     *   PS. The defailt one  :)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    /**
+     *  Message two words test
+     */
+    public void testMessage()
+    {
+	hello.MyHandler h = new hello.MyHandler();
+	String message = h.getMessage();
+	String [] words = message.split(" ");
+	assertEquals("The first word should be 'Hello'","Hello,",words[0]);
+	assertEquals("The first word should be 'Hello'","HTTP!",words[1]);
+    }
+
+
+}
